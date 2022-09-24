306610f024ea1d2566650c75b513b85c3cf1bfbf
Simon Byrne <simonbyrne@gmail.com>
2022-09-24 14:34:17 -0700

    add artifact and annotation support (#2)

--- src/BuildkiteUtils.jl
+++ src/BuildkiteUtils.jl
@@ -7,22 +7,74 @@ function __init__()
     AGENT_BINARY[] = joinpath(get(ENV, "BUILDKITE_BIN_PATH", ""), "buildkite-agent")
 end
 
+
+# meta-data dictionary
 struct MetaDataDict <: AbstractDict{String, String}
 end
-
 const METADATA = MetaDataDict()
 
-function Base.getindex(::MetaDataDict, key::String)
+function Base.getindex(::MetaDataDict, key::AbstractString)
     read(`$(agent()) meta-data get $key`, String)
 end
-function Base.setindex!(::MetaDataDict, value, key::String)
+function Base.get(::MetaDataDict, key::AbstractString, default::AbstractString)
+    read(`$(agent()) meta-data get $key --default $default`, String)
+end
+function Base.setindex!(::MetaDataDict, value, key::AbstractString)
     run(pipeline(`$(agent()) meta-data set $key`, stdin=IOBuffer(string(value))))
 end
-function Base.haskey(::MetaDataDict, key::String)
+function Base.haskey(::MetaDataDict, key::AbstractString)
     success(`$(agent()) meta-data exists $key`)
 end
 function Base.keys(::MetaDataDict)
     readlines(`$(agent()) meta-data keys`)
+end
+
+
+# artifact
+
+function artifact_upload(pattern::AbstractString)
+    run(`$(agent()) artifact upload $pattern`)
+end
+
+function artifact_search(pattern::AbstractString="*"; step=nothing, build=nothing)
+    format = "%p\n"
+    cmd = `$(agent()) artifact search  --format $format`
+    if !isnothing(step)
+        cmd = `$cmd --step $step`
+    end
+    if !isnothing(build)
+        cmd = `$cmd --build $build`
+    end
+    cmd = `$cmd $pattern`
+    readlines(ignorestatus(cmd))
+end
+
+function artifact_download(pattern::AbstractString, destination::AbstractString="."; step=nothing, build=nothing)
+    cmd = `$(agent()) artifact download`
+    if !isnothing(step)
+        cmd = `$cmd --step $step`
+    end
+    if !isnothing(build)
+        cmd = `$cmd --build $build`
+    end
+    cmd = `$cmd $pattern $destination`
+    run(cmd)
+end
+
+# annotation
+
+function annotate(data; context=nothing, style=nothing, append=false)
+    cmd = `$(agent()) annotate`
+    if !isnothing(context)
+        cmd = `$cmd --context $context`
+    end
+    if !isnothing(style)
+        cmd = `$cmd --style $style`
+    end
+    if append
+        cmd = `$cmd --append`
+    end
+    run(pipeline(cmd, stdin=IOBuffer(string(data))))
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,53 @@
 using BuildkiteUtils, Test
 
+
 @test success(`$(BuildkiteUtils.agent()) --version`)
 
-@test isempty(keys(BuildkiteUtils.METADATA))
+@testset "meta-data" begin
+    @test isempty(keys(BuildkiteUtils.METADATA))
 
-BuildkiteUtils.METADATA["aa"] = "hello"
+    BuildkiteUtils.METADATA["aa"] = "hello"
 
-@test keys(BuildkiteUtils.METADATA) == ["aa"]
-@test BuildkiteUtils.METADATA["aa"] == "hello"
+    @test keys(BuildkiteUtils.METADATA) == ["aa"]
+    @test BuildkiteUtils.METADATA["aa"] == "hello"
+end
 
+using Plots
+
+@testset "artifact" begin
+
+    @test BuildkiteUtils.artifact_search("*") == []
+
+    p = plot(identity, sin, -2pi, 2pi)
+    dir = mktempdir()
+    png(p, joinpath(dir, "sin x.png"))
+
+    subdir = joinpath(dir, "extra")
+    mkpath(subdir)
+    write(joinpath(subdir, "hello.txt"), "hello world")
+
+    cd(dir) do
+        BuildkiteUtils.artifact_upload("*.png")
+        BuildkiteUtils.artifact_upload("**/*.txt")
+    end
+
+    @test sort(BuildkiteUtils.artifact_search()) == sort(["sin x.png", "extra/hello.txt"])
+
+    newdir = mktempdir()
+    BuildkiteUtils.artifact_download("*.png", newdir)
+    @test readdir(newdir) == ["sin x.png"]
+    @test read(joinpath(dir, "sin x.png")) == read(joinpath(newdir, "sin x.png"))
+end
+
+@testset "annotation" begin
+    BuildkiteUtils.annotate("""
+    Default annotation
+
+    <img src="artifact://sin x.png" alt="sin(x)" height=250 >
+
+    """)
+
+    BuildkiteUtils.annotate("Success"; style="success", context="xtra")
+
+    BuildkiteUtils.annotate("and some more"; append=true)
+end
