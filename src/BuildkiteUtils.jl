f544ebedde8866739d4d67747e3a1af42f39e6fb
Simon Byrne <simonbyrne@gmail.com>
2022-09-23 23:14:16 -0700

    add artifact support

--- src/BuildkiteUtils.jl
+++ src/BuildkiteUtils.jl
@@ -7,22 +7,43 @@ function __init__()
     AGENT_BINARY[] = joinpath(get(ENV, "BUILDKITE_BIN_PATH", ""), "buildkite-agent")
 end
 
+
+# meta-data dictionary
 struct MetaDataDict <: AbstractDict{String, String}
 end
-
 const METADATA = MetaDataDict()
 
-function Base.getindex(::MetaDataDict, key::String)
+function Base.getindex(::MetaDataDict, key::AbstractString)
     read(`$(agent()) meta-data get $key`, String)
 end
-function Base.setindex!(::MetaDataDict, value, key::String)
+function Base.get(::MetaDataDict, key::AbstractString, default::AbstractString)
+    read(`$(agent()) meta-data get $key --default $default`, String)
+end
+function Base.setindex!(::MetaDataDict, value, key::AbstractString)
     run(pipeline(`$(agent()) meta-data set $key`, stdin=IOBuffer(string(value))))
 end
-function Base.haskey(::MetaDataDict, key::String)
+function Base.haskey(::MetaDataDict, key::AbstractString)
     success(`$(agent()) meta-data exists $key`)
 end
 function Base.keys(::MetaDataDict)
     readlines(`$(agent()) meta-data keys`)
+end
+
+
+# artifact
+
+function artifact_upload(pattern::AbstractString)
+    run(`$(agent()) artifact upload $pattern`)
+end
+function artifact_download(pattern::AbstractString, destination::AbstractString="."; step=nothing, build=nothing)
+    cmd = `$(agent()) artifact upload $pattern`
+    if !isnothing(step)
+        cmd = `$cmd --step $step`
+    end
+    if !isnothing(build)
+        cmd = `$cmd --build $build`
+    end
+    run(cmd)
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,29 @@ using BuildkiteUtils, Test
 
 @test success(`$(BuildkiteUtils.agent()) --version`)
 
-@test isempty(keys(BuildkiteUtils.METADATA))
+@testset "meta-data" begin
+    @test isempty(keys(BuildkiteUtils.METADATA))
 
-BuildkiteUtils.METADATA["aa"] = "hello"
+    BuildkiteUtils.METADATA["aa"] = "hello"
 
-@test keys(BuildkiteUtils.METADATA) == ["aa"]
-@test BuildkiteUtils.METADATA["aa"] == "hello"
+    @test keys(BuildkiteUtils.METADATA) == ["aa"]
+    @test BuildkiteUtils.METADATA["aa"] == "hello"
+end
+
+using Plots
+
+@testset "artifact" begin
+    p = plot(sin, (-3,3))
+    dir = mktempdir()
+    png(p, joinpath(dir, "sinx.png"))
+
+    BuildkiteUtils.artifact_upload("*.png")
+
+    newdir = mktempdir()
+    BuildkiteUtils.artifact_download("*.png", newdir)
+    @test readdir(newdir) == ["sinx.png"]
+    @test read(joinpath(dir, "sinx.png")) == read(joinpath(newdir, "sinx.png"))
+end
+
+
 
