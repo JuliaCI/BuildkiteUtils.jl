0dc9c4240520a3d644b9ea06126aaaec6b16a8eb
Simon Byrne <simonbyrne@gmail.com>
2022-09-28 21:43:33 -0700

    build docs

---
+++ docs/src/index.md
@@ -0,0 +1,23 @@
+# BuildkiteUtils.jl
+
+BuildkiteUtils is a collection of utility functions when running jobs on [Buildkite](https://buildkite.com).
+
+## Build meta-data
+
+```@docs
+BuildkiteUtils.METADATA
+```
+
+## Artifacts
+
+```@docs
+BuildkiteUtils.artifact_upload
+BuildkiteUtils.artifact_download
+BuildkiteUtils.artifact_search
+```
+
+## Annotations
+
+```@docs
+BuildkiteUtils.annotate
+```
--- src/BuildkiteUtils.jl
+++ src/BuildkiteUtils.jl
@@ -7,10 +7,22 @@ function __init__()
     AGENT_BINARY[] = joinpath(get(ENV, "BUILDKITE_BIN_PATH", ""), "buildkite-agent")
 end
 
-
 # meta-data dictionary
 struct MetaDataDict <: AbstractDict{String, String}
 end
+
+"""
+    BuildkiteUtils.METADATA
+
+A dict-like object for setting and getting [build
+meta-data](https://buildkite.com/docs/pipelines/build-meta-data)
+
+```
+x = BuildkiteUtils.METADATA[key]   # get meta-data
+BuildkiteUtils.METADATA[key] = val # set meta-data
+keys(BuildkiteUtils.METADATA)      # list of meta-data keys
+haskey(BuildkiteUtils.METADATA)    # check if key exists
+"""
 const METADATA = MetaDataDict()
 
 function Base.getindex(::MetaDataDict, key::AbstractString)
@@ -31,11 +43,28 @@ end
 
 
 # artifact
+"""
+    BuildkiteUtils.artifact_upload(pattern::AbstractString)
 
+Upload all artifacts in the current directory matching `pattern`.
+
+See [Uploading artifacts](https://buildkite.com/docs/agent/v3/cli-artifact#uploading-artifacts).
+"""
 function artifact_upload(pattern::AbstractString)
     run(`$(agent()) artifact upload $pattern`)
 end
 
+"""
+    BuildkiteUtils.artifact_search(pattern::AbstractString; [step,] [build])
+
+List all uploaded artifacts matching `pattern`.
+
+Optional keyword arguments
+ - `step` will limit to artifacts in a given `step` (either the key or the step
+ID),
+ - `build` allows downloading artifact from a given build ID.
+
+"""
 function artifact_search(pattern::AbstractString="*"; step=nothing, build=nothing)
     format = "%p\n"
     cmd = `$(agent()) artifact search  --format $format`
@@ -49,6 +78,18 @@ function artifact_search(pattern::AbstractString="*"; step=nothing, build=nothin
     readlines(ignorestatus(cmd))
 end
 
+"""
+    BuildkiteUtils.artifact_download(pattern::AbstractString, destination::AbstractString=".";
+       [step,] [build])
+
+Download all artifacts matching `pattern` to `destination`.
+
+Optional keyword arguments
+ - `step` will limit to artifacts in a given `step` (either the key or the step
+ID),
+ - `build` allows downloading artifact from a given build ID.
+
+"""
 function artifact_download(pattern::AbstractString, destination::AbstractString="."; step=nothing, build=nothing)
     cmd = `$(agent()) artifact download`
     if !isnothing(step)
@@ -62,7 +103,24 @@ function artifact_download(pattern::AbstractString, destination::AbstractString=
 end
 
 # annotation
+"""
+    BuildkiteUtils.annotate(data; context=nothing, style=nothing, append=false)
 
+Annotates the current build with `data`: this is a Markdown-formatted string.
+
+- `context`: a unique identifier for a given annotation. The default will use
+  the default context.
+- `style`: one of
+  - `nothing` (default style)
+  - `"success"`
+  - `"info"`
+  - `"warning"`
+  - `"error"`
+- `append`: if `true`, will add to the existing annotation with the same
+  context, otherwise will replace it.
+
+See [`buildkite-agent annotate`](https://buildkite.com/docs/agent/v3/cli-annotate) for more information.
+"""
 function annotate(data; context=nothing, style=nothing, append=false)
     cmd = `$(agent()) annotate`
     if !isnothing(context)
